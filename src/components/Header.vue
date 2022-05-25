088b1b98d2c2a76698da876c6630797d3ae1eacf
Ftm-amani <ftm.amani95@gmail.com>
2022-05-25 14:44:28 +0430

    add components and functionalities - routs - http requests - vuex

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -34,3 +34,40 @@
     </div><!-- /.container-fluid -->
   </nav>
 </template>
+
+<script>
+import {mapActions} from 'vuex';
+
+export default {
+  data() {
+    return {
+      isDropdownOpen: false
+    }
+  },
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    }
+  },
+  methods: {
+    ...mapActions({
+      randomizeStocks: 'randomizeStocks',
+      fetchData: 'loadData'
+    }),
+    endDay() {
+      this.randomizeStocks();
+    },
+    saveData() {
+      const data = {
+        funds: this.$store.getters.funds,
+        stockPortfolio: this.$store.getters.stockPortfolio,
+        stocks: this.$store.getters.stocks
+      };
+      this.$http.put('data.json', data);
+    },
+    loadData() {
+      this.fetchData();
+    }
+  }
+}
+</script>
